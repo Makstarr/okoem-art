2684fdeea5843d42b92aaee6862d64ef40afceec
Makstarr <maksi112/@>
2020-10-17 01:45:13 +0300

    Добавил смену текста

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ let objects=$('.objects')[0]
 let goals=document.getElementsByClassName('txt-goal')
 let authors=document.getElementsByClassName('txt-author')
 
-console.log(authors)
+console.log(objects)
 
 objects.onclick = function(event) {
     let target = event.target; // где был клик?
+    console.log(target)
     if (target.tagName != 'BUTTON') return; // не на TD? тогда не интересует
     if ($(target).hasClass("checked")) return;
     $(target).siblings(".checked").toggleClass('checked')
