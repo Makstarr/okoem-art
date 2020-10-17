5c397fd0eba6ec91fee0f49bf0c7e9c4342c9db8
Makstarr <maksi112/@>
2020-10-17 17:34:00 +0300

    Убрал границы на кнопках

--- js/main.js
+++ js/main.js
@@ -1,21 +1,16 @@
-let objects=$('.objects')[0]
-let goals=document.getElementsByClassName('txt-goal')
-let authors=document.getElementsByClassName('txt-author')
+let objects = $('.objects')[0]
+let goals = document.getElementsByClassName('txt-goal')
+let authors = document.getElementsByClassName('txt-author')
 
-console.log(objects)
-
-objects.onclick = function(event) {
-    let target = event.target; // где был клик?
-    console.log(target)
-    if (target.tagName != 'BUTTON') return; // не на TD? тогда не интересует
+objects.onclick = function (event) {
+    let target = event.target;
+    "use strict";
+    if (target.tagName != 'BUTTON') return;
     if ($(target).hasClass("checked")) return;
     $(target).siblings(".checked").toggleClass('checked')
     $(target).toggleClass('checked')
-    console.log(goals[$(target).attr('class')[0]])
-    console.log(authors[$(target).attr('class')[0]])
     $(goals[$(target).attr('class')[0]]).toggleClass('hidden')
-    $(goals[$(target).attr('class')[0]]).toggleClass('visible') 
-    $(authors[$(target).attr('class')[0]]).toggleClass('hidden') 
-    $(authors[$(target).attr('class')[0]]).toggleClass('visible') 
-     // подсветить TD
-  };
+    $(goals[$(target).attr('class')[0]]).toggleClass('visible')
+    $(authors[$(target).attr('class')[0]]).toggleClass('hidden')
+    $(authors[$(target).attr('class')[0]]).toggleClass('visible')
+};
